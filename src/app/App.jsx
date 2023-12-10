cef6f22bb4194cc9eab27fec57f1ae91cc0a40ca
Suvi Sulonen <suvi.sulonen@gmail.com>
2023-12-10 17:29:22 +0200

    Refactor App to include TaskList component into it

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,6 +4,7 @@
 import { Container } from "react-bootstrap";
 import { Footer } from "../common/Footer";
 import { Header } from "../common/Header";
+import { TaskList } from "../features/tasks/TaskList";
 
 // Styles
 import "./App.css";
@@ -12,6 +13,7 @@ function App() {
     return (
         <Container fluid>
             <Header />
+            <TaskList />
             <Footer />
         </Container>
     );
