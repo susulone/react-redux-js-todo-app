cec1cc9bea61f6cb587e35238eeb65d707ac36c2
Suvi Sulonen <suvi.sulonen@gmail.com>
2023-12-10 16:45:02 +0200

    Configure redux store and connect it to the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// React
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Redux
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+
+// Components
+import App from "./app/App";
+
+// Styling
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
+);
