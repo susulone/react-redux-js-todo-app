5e4705de35b2c9307854ad70dddad3870f20f855
Suvi Sulonen <suvi.sulonen@gmail.com>
2023-12-10 17:28:16 +0200

    Add TaskList component

---
+++ src/features/tasks/TaskList.jsx
@@ -0,0 +1,29 @@
+// React
+
+// Redux
+import { useSelector } from "react-redux";
+import { selectAllTasks } from "./taskSlice";
+
+// Components
+import { TaskItem } from "./TaskItem";
+
+// Styles
+import "./styles.css";
+
+export const TaskList = () => {
+    const tasks = useSelector(selectAllTasks);
+    console.log(tasks);
+
+    return (
+        <main id="task-list">
+            {tasks.map((task) => (
+                <TaskItem
+                    key={task.id}
+                    id={task.id}
+                    task={task.task}
+                    completed={task.completed}
+                />
+            ))}
+        </main>
+    );
+};
